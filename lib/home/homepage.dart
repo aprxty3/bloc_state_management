ec2208ad277b777474e658f49c46af7434dbf4f9
aprxty3 <ajiprxty3@gmail.com>
2023-03-26 03:30:01 +0700

    named route access

--- lib/home/homepage.dart
+++ lib/home/homepage.dart
@@ -18,14 +18,7 @@ class HomePageUi extends StatelessWidget {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          Navigator.of(context).push(
-            MaterialPageRoute(
-              builder: (context) => BlocProvider.value(
-                value: counter,
-                child: const OtherPage(),
-              ),
-            ),
-          );
+          Navigator.pushNamed(context, '/other');
         },
         child: const Icon(Icons.forward),
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -3,20 +3,35 @@ import 'package:bloc_state_management/home/homepage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import 'other/other.dart';
+
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
+  final Counter counter = Counter();
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: BlocProvider(
-        create: (context) => Counter(),
-        child: HomePageUi(),
-      ),
+      // home: BlocProvider(
+      //   create: (context) => Counter(),
+      //   child: HomePageUi(),
+      // ),
+
+      initialRoute: '/',
+      routes: {
+        '/': (context) => BlocProvider.value(
+              value: counter,
+              child: HomePageUi(),
+            ),
+        '/other': (context) => BlocProvider.value(
+              value: counter,
+              child: OtherPage(),
+            ),
+      },
     );
   }
 }
